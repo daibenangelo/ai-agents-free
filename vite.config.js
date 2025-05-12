da89e78f44fcd0b101e2c9e57fd549a8d8fbc28b
daibenangelo <daibenangelosanchez@gmail.com>
2025-05-12 16:10:00 +0800

    bootstrap

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      'bootstrap/dist/css/bootstrap.min.css': 'bootstrap/dist/css/bootstrap.min.css'
+    }
+  },
+  optimizeDeps: {
+    include: ['bootstrap']
+  }
 })
